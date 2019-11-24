bf7ab10220e22069682636f1fabc63132010b7a6
Paris DOUADY <paris.douady@hotmail.fr>
2019-11-24 18:59:38 +0100

    test iterators

--- src/main.rs
+++ src/main.rs
@@ -7,34 +7,13 @@ use ggez::graphics::*;
 use ggez::input::keyboard::{KeyCode, KeyMods};
 use ggez::*;
 
+use rand::rngs::SmallRng;
+use rand::{Rng, SeedableRng};
+
 mod camera;
+mod car;
 mod gsb;
 mod shape_render;
-
-struct State {
-    text: graphics::Text,
-    gsb: GSB,
-    time: f32,
-}
-
-impl State {
-    fn new(ctx: &mut Context) -> GameResult<State> {
-        // The ttf file will be in your resources directory. Later, we
-        // will mount that directory so we can omit it in the path here.
-
-        println!("{}", filesystem::resources_dir(ctx).display());
-
-        let font = graphics::Font::new(ctx, "/bmonofont-i18n.ttf")?;
-        let text = graphics::Text::new(("Hello world!", font, 48.0));
-
-        graphics::set_resizable(ctx, true)?;
-        Ok(State {
-            text,
-            gsb: gsb::GSB::new(),
-            time: 0.,
-        })
-    }
-}
 
 fn draw_text<P>(ctx: &mut Context, text: &graphics::Text, pos: P) -> GameResult<()>
 where
@@ -46,9 +25,59 @@ where
     graphics::draw(ctx, text, trans)
 }
 
+fn draw_image<P>(ctx: &mut Context, image: &graphics::Image, pos: P) -> GameResult<()>
+where
+    P: Into<mint::Point2<f32>>,
+{
+    let mut new_pos = pos.into();
+    new_pos.y += image.height() as f32;
+    let trans = graphics::DrawParam::new().dest(new_pos).scale([1., -1.]);
+    graphics::draw(ctx, image, trans)
+}
+
+struct State {
+    text: graphics::Text,
+    gsb: GSB,
+    time: f32,
+    cars: Vec<car::Car>,
+    test: graphics::Image,
+}
+
+impl State {
+    fn new(ctx: &mut Context) -> GameResult<State> {
+        println!("{}", filesystem::resources_dir(ctx).display());
+
+        let font = graphics::Font::new(ctx, "/bmonofont-i18n.ttf")?;
+        let text = graphics::Text::new(("Hello world!", font, 48.0));
+        let test: Image = graphics::Image::new(ctx, "/test.png")?;
+
+        graphics::set_resizable(ctx, true)?;
+        Ok(State {
+            text,
+            gsb: gsb::GSB::new(),
+            time: 0.,
+            cars: (0..100000)
+                .into_iter()
+                .map(|_| car::Car {
+                    position: [0., 0.].into(),
+                })
+                .collect(),
+            test,
+        })
+    }
+}
+
 impl ggez::event::EventHandler for State {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-        self.time += timer::delta(ctx).as_secs_f32();
+        let delta = timer::delta(ctx).as_secs_f32();
+        self.time += delta;
+
+        let mut rng = SmallRng::from_entropy();
+
+        for c in self.cars.iter_mut() {
+            c.position.x = rng.gen::<f32>() * 1000.;
+            c.position.y = rng.gen::<f32>() * 1000.;
+        }
         Ok(())
     }
 
@@ -57,25 +86,20 @@ impl ggez::event::EventHandler for State {
         self.gsb.easy_camera_movement(ctx);
         self.gsb.update(ctx);
 
-        let lol = self.gsb.unproject_mouse_click(ctx);
+        let _lol = self.gsb.unproject_mouse_click(ctx);
 
         let x = 50.0 * (self.time as f32).cos();
         let y = 50.0 * (1.5 * self.time as f32 + 0.5).sin();
 
         let mut sr = ShapeRenderer::begin();
-        //sr.mode = DrawMode::fill();
-
-        /*
-        for _i in 0..100 {
-            sr.draw_rect(lol, 10., 10.);
-            sr.draw_rect([x, y], 10., 10.);
+        sr.color = graphics::WHITE;
+        //sr.mode = DrawMode::stroke(1.0);
+        for c in self.cars.iter() {
+            sr.draw_rect(c.position, 1., 1.);
         }
-        */
-        sr.draw_rect_skinny([x, y], 10., 10.);
-        sr.draw_rect_skinny(lol, 10., 10.);
         sr.end(ctx)?;
-
         draw_text(ctx, &self.text, [0., 0.])?;
+        draw_image(ctx, &self.test, [0.0, 0.0])?;
         graphics::present(ctx)
     }
 
