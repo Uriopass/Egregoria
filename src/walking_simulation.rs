c85688797d885c8d26894ea6a4aad6f05f1a1564
Paris Douady <paris.douady@hotmail.fr>
2019-12-03 09:45:16 +0100

    Add vecmath lib and add human manager

--- src/walking_simulation.rs
+++ src/walking_simulation.rs
@@ -1,13 +1,26 @@
-use ggez::nalgebra::Vector2;
+use vecmath::Vector2;
 
 struct Human {
     position: Vector2<f32>,
+    direction: Vector2<f32>,
 }
 
 impl Default for Human {
     fn default() -> Self {
         Human {
-            position: Vector2::new(0., 0.),
+            position: [0., 0.].into(),
+            direction: [1., 0.].into(),
         }
+    }
+}
+
+#[derive(Default)]
+struct HumanManager {
+    humans: Vec<Human>,
+}
+
+impl HumanManager {
+    fn update(&mut self, delta: f32) {
+        //
     }
 }
