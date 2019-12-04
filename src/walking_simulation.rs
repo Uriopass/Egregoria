a1087bef271847c4b629b7bf2cb47b3364b9f51b
Douady Pâris <paris.douady@hotmail.fr>
2019-12-04 16:49:17 +0100

    Add simulation skeleton

--- src/walking_simulation.rs
+++ src/walking_simulation.rs
@@ -1,31 +1,58 @@
 use crate::shape_render::ShapeRenderer;
-use cgmath::{EuclideanSpace, Point2, Vector2, Zero};
+use cgmath::{Point2, Vector2};
 
+type Vector2f = Vector2<f32>;
+
+#[allow(dead_code)]
 struct Human {
     position: Point2<f32>,
-    direction: Vector2<f32>,
+    speed: Vector2f,
+    direction: Vector2f,
 }
 
 impl Default for Human {
     fn default() -> Self {
         Human {
-            position: Point2::<f32>::origin(),
+            position: [0., 0.].into(),
+            speed: [0., 0.].into(),
             direction: [1., 0.].into(),
         }
     }
 }
 
-#[derive(Default)]
-struct HumanManager {
+impl Human {
+    fn calc_acceleration(&self) -> Vector2f {
+        [rand::random::<f32>(), rand::random::<f32>()].into()
+    }
+}
+
+pub struct HumanManager {
     humans: Vec<Human>,
 }
 
 impl HumanManager {
-    fn update(&mut self, delta: f32) {
-        //
+    pub fn new(n_humans: i32) -> Self {
+        HumanManager {
+            humans: (0..n_humans)
+                .map(|_| Human {
+                    position: [rand::random::<f32>() * 1000., rand::random::<f32>() * 1000.].into(),
+                    speed: [0., 0.].into(),
+                    direction: [1., 0.].into(),
+                })
+                .collect(),
+        }
     }
 
-    fn draw(&self, sr: &mut ShapeRenderer) {
-        for human in self.humans.iter() {}
+    pub fn update(&mut self, delta: f32) {
+        for h in self.humans.iter_mut() {
+            let acc = h.calc_acceleration();
+            h.speed += acc * delta;
+        }
+    }
+
+    pub fn draw(&self, sr: &mut ShapeRenderer) {
+        for human in self.humans.iter() {
+            sr.draw_circle([human.position.x, human.position.y], 10.);
+        }
     }
 }
