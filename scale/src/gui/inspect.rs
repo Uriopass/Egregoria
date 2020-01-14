6797c9d7c6d6add927bc8633fcb6bfc6a3b2b069
Paris DOUADY <paris.douady@hotmail.fr>
2020-01-14 09:23:03 +0100

    Add imgui vector2 and empty struct impl

--- scale/src/gui/inspect.rs
+++ scale/src/gui/inspect.rs
@@ -1,26 +1,57 @@
+use crate::interaction::{Movable, Selectable};
 use crate::physics::physics_components::Transform;
 use cgmath::Vector2;
 use imgui::Ui;
-use imgui_inspect::{InspectArgsDefault, InspectRenderDefault};
+use imgui_inspect::{get_same_or_none, InspectArgsDefault, InspectRenderDefault};
 use imgui_inspect_derive::*;
 use specs::prelude::*;
 
-#[derive(Inspect)]
-struct Vec2 {
-    x: f32,
-    y: f32,
-}
+struct ImCgVec2;
+impl InspectRenderDefault<Vector2<f32>> for ImCgVec2 {
+    fn render(data: &[&Vector2<f32>], label: &'static str, ui: &Ui, args: &InspectArgsDefault) {
+        let xs: Vec<&f32> = data.iter().map(|x| &x.x).collect();
+        let ys: Vec<&f32> = data.iter().map(|x| &x.y).collect();
+        <f32 as InspectRenderDefault<f32>>::render(xs.as_slice(), "x", ui, args);
+        <f32 as InspectRenderDefault<f32>>::render(ys.as_slice(), "y", ui, args);
+    }
 
-impl From<Vector2<f32>> for Vec2 {
-    fn from(v: Vector2<f32>) -> Self {
-        Self { x: v.x, y: v.y }
+    fn render_mut(
+        data: &mut [&mut Vector2<f32>],
+        label: &'static str,
+        ui: &Ui,
+        args: &InspectArgsDefault,
+    ) -> bool {
+        let mut xs: Vec<&mut f32> = data.into_iter().map(|x| &mut (*x).x).collect();
+        let changed =
+            <f32 as InspectRenderDefault<f32>>::render_mut(xs.as_mut_slice(), "x", ui, args);
+
+        let mut ys: Vec<&mut f32> = data.into_iter().map(|x| &mut (*x).y).collect();
+        let changed =
+            <f32 as InspectRenderDefault<f32>>::render_mut(ys.as_mut_slice(), "y", ui, args)
+                || changed;
+        changed
     }
 }
 
-impl Into<Vector2<f32>> for Vec2 {
-    fn into(self) -> Vector2<f32> {
-        [self.x, self.y].into()
-    }
+#[macro_export]
+macro_rules! empty_struct_inspect_impl {
+    ($x : ty) => {
+        impl InspectRenderDefault<$x> for $x {
+            fn render(_: &[&$x], _: &'static str, ui: &Ui, _: &InspectArgsDefault) {
+                ui.text(std::stringify!($x))
+            }
+
+            fn render_mut(
+                _: &mut [&mut $x],
+                _: &'static str,
+                ui: &Ui,
+                _: &InspectArgsDefault,
+            ) -> bool {
+                ui.text(std::stringify!($x));
+                false
+            }
+        }
+    };
 }
 
 pub struct InspectRenderer<'a, 'b> {
@@ -47,14 +78,16 @@ impl<'a, 'b> InspectRenderer<'a, 'b> {
             .write_component::<Transform>()
             .get_mut(self.entity)
         {
-            let mut position = Vec2::from(x.get_position());
-            <Vec2 as InspectRenderDefault<Vec2>>::render_mut(
+            let mut position = x.get_position();
+            <ImCgVec2 as InspectRenderDefault<Vector2<f32>>>::render_mut(
                 &mut [&mut position],
-                "generated_label",
+                "Transform",
                 self.ui,
                 &InspectArgsDefault::default(),
             );
-            x.set_position(position.into());
+            x.set_position(position);
         }
+
+        self.inspect_component::<Movable>();
     }
 }
--- scale/src/gui/mod.rs
+++ scale/src/gui/mod.rs
@@ -13,29 +13,19 @@ impl TestGui {
     pub fn render(&self, ui: &Ui, world: &mut World) {
         let selected = *world.read_resource::<SelectedEntity>();
         // Window
-        imgui::Window::new(im_str!("Hello world"))
-            .size([300.0, 600.0], imgui::Condition::FirstUseEver)
-            .position([100.0, 100.0], imgui::Condition::FirstUseEver)
-            .build(&ui, || {
-                ui.text(im_str!("Hello world!"));
-                ui.text(im_str!("こんにちは世界！"));
-                ui.text(im_str!("This...is...imgui-rs!"));
-                ui.separator();
-
-                if let Some(e) = selected.0 {
-                    ui.text(im_str!("Selected entity"));
+        if let Some(e) = selected.0 {
+            imgui::Window::new(im_str!("Inspect"))
+                .size([200.0, 300.0], imgui::Condition::FirstUseEver)
+                .position([30.0, 30.0], imgui::Condition::FirstUseEver)
+                .build(&ui, || {
                     crate::gui::inspect::InspectRenderer {
                         world,
                         entity: e,
                         ui,
                     }
                     .render();
-                }
-
-                if ui.small_button(im_str!("small button")) {
-                    println!("Small button clicked");
-                }
-            });
+                });
+        }
 
         // Menu bar
         ui.main_menu_bar(|| {
