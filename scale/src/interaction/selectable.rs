585adfc941d68fb237428801beea85e18529873c
Paris DOUADY <paris.douady@hotmail.fr>
2020-01-14 11:11:01 +0100

    Add visual hint to selected entity

--- scale/src/interaction/selectable.rs
+++ scale/src/interaction/selectable.rs
@@ -1,10 +1,13 @@
 use crate::empty_struct_inspect_impl;
 use crate::engine_interaction::{MouseButton, MouseInfo};
 use crate::physics::physics_components::Transform;
+use crate::rendering::meshrender_component::{CircleRender, MeshRenderComponent, MeshRenderEnum};
+use crate::rendering::Color;
 use cgmath::InnerSpace;
 use imgui::Ui;
 use imgui_inspect::{InspectArgsDefault, InspectRenderDefault};
 use specs::prelude::*;
+use specs::shred::DynamicSystemData;
 use specs::Component;
 use std::f32;
 
@@ -17,18 +20,24 @@ empty_struct_inspect_impl!(Selectable);
 pub struct SelectedEntity(pub Option<Entity>);
 
 #[derive(Default)]
-pub struct SelectableSystem;
+pub struct SelectableSystem {
+    aura: Option<Entity>,
+}
 
 impl<'a> System<'a> for SelectableSystem {
     type SystemData = (
         Entities<'a>,
         Read<'a, MouseInfo>,
         Write<'a, SelectedEntity>,
-        ReadStorage<'a, Transform>,
+        WriteStorage<'a, Transform>,
         ReadStorage<'a, Selectable>,
+        WriteStorage<'a, MeshRenderComponent>,
     );
 
-    fn run(&mut self, (entities, mouse, mut selected, transforms, selectables): Self::SystemData) {
+    fn run(
+        &mut self,
+        (entities, mouse, mut selected, mut transforms, selectables, mut meshrenders): Self::SystemData,
+    ) {
         if mouse.just_pressed.contains(&MouseButton::Left) {
             let mut min_dist = f32::MAX;
             let mut closest = None;
@@ -41,6 +50,40 @@ impl<'a> System<'a> for SelectableSystem {
             }
             *selected = SelectedEntity(closest);
             println!("Selected new entity");
+
+            meshrenders
+                .get_mut(self.aura.unwrap())
+                .map(|mr| match &mut mr.orders[0] {
+                    MeshRenderEnum::Circle(x) => {
+                        x.radius = if closest.is_some() { 3.0 } else { 0.0 };
+                    }
+                    _ => (),
+                });
         }
+
+        if let Some(sel) = selected.0 {
+            let pos = transforms.get(sel).unwrap().get_position();
+
+            transforms
+                .get_mut(self.aura.unwrap())
+                .unwrap()
+                .set_position(pos)
+        }
+    }
+
+    fn setup(&mut self, world: &mut World) {
+        <Self::SystemData as DynamicSystemData>::setup(&self.accessor(), world);
+        self.aura = Some(
+            world
+                .create_entity()
+                .with(Transform::zero())
+                .with(MeshRenderComponent::from(CircleRender {
+                    offset: [0.0, 0.0].into(),
+                    filled: false,
+                    color: Color::gray(0.7),
+                    radius: 0.0,
+                }))
+                .build(),
+        );
     }
 }
