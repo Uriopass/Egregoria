f7fb3758453e2bf23fb06324ccd71fce2b982d61
Paris Douady <paris.douady@hotmail.fr>
2020-01-15 15:25:57 +0100

    Fix big physics bug and make collisions enabled by default

--- scale/src/cars/mod.rs
+++ scale/src/cars/mod.rs
@@ -40,7 +40,7 @@ pub fn setup(world: &mut World) {
     g.add_to_world(world);
     world.insert(g);
 
-    for _i in 0..30 {
+    for _i in 0..500 {
         make_car_entity(
             world,
             200.0 * Vector2::<f32>::new(rand::random::<f32>() - 0.5, rand::random::<f32>() - 0.5),
