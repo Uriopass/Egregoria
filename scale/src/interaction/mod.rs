d9c191ccd35f383e4a39ee5dd14f27a300db4d40
Paris DOUADY <paris.douady@hotmail.fr>
2020-01-21 11:51:18 +0100

    Simplify and split selectable/movable code

--- scale/src/interaction/mod.rs
+++ scale/src/interaction/mod.rs
@@ -1,5 +1,7 @@
 pub use self::movable::*;
 pub use self::selectable::*;
+pub use self::selectable_aura::*;
 
 mod movable;
 mod selectable;
+mod selectable_aura;
---
+++ scale/src/interaction/selectable_aura.rs
@@ -0,0 +1,55 @@
+use crate::interaction::SelectedEntity;
+use crate::physics::physics_components::Transform;
+use crate::rendering::meshrender_component::{CircleRender, MeshRender};
+use crate::rendering::Color;
+use specs::prelude::*;
+use specs::shred::DynamicSystemData;
+
+#[derive(Default)]
+pub struct SelectableAuraSystem {
+    aura: Option<Entity>,
+}
+
+impl<'a> System<'a> for SelectableAuraSystem {
+    type SystemData = (
+        Read<'a, SelectedEntity>,
+        WriteStorage<'a, Transform>,
+        WriteStorage<'a, MeshRender>,
+    );
+
+    fn run(&mut self, (selected, mut transforms, mut meshrenders): Self::SystemData) {
+        meshrenders.get_mut(self.aura.unwrap()).unwrap().hide = true;
+
+        if let Some(pos) = selected
+            .0
+            .and_then(|sel| transforms.get(sel).map(|x| x.position()))
+        {
+            transforms
+                .get_mut(self.aura.unwrap())
+                .unwrap()
+                .set_position(pos);
+            meshrenders.get_mut(self.aura.unwrap()).unwrap().hide = false;
+        }
+    }
+
+    fn setup(&mut self, world: &mut World) {
+        <Self::SystemData as DynamicSystemData>::setup(&self.accessor(), world);
+        let mut mr = MeshRender::simple(
+            CircleRender {
+                offset: [0.0, 0.0].into(),
+                filled: false,
+                color: Color::gray(0.7),
+                radius: 3.0,
+            },
+            9,
+        );
+        mr.hide = true;
+        self.aura = Some(
+            world
+                .create_entity()
+                .with(Transform::zero())
+                .with(mr)
+                .build(),
+        );
+    }
+}
