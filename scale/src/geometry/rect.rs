8db792a1584d280ac65d17f8913c460252a4ba86
Paris DOUADY <paris.douady@hotmail.fr>
2020-02-15 13:03:31 +0100

    Add gridstore and rect

---
+++ scale/src/geometry/rect.rs
@@ -0,0 +1,127 @@
+use cgmath::Vector2;
+
+#[derive(Clone)]
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub w: f32,
+    pub h: f32,
+}
+
+#[allow(dead_code)]
+impl Rect {
+    /// Create a new `Rect`.
+    pub const fn new(x: f32, y: f32, w: f32, h: f32) -> Self {
+        Rect { x, y, w, h }
+    }
+
+    /// Creates a new `Rect` a la Love2D's `love.graphics.newQuad`,
+    /// as a fraction of the reference rect's size.
+    pub fn fraction(x: f32, y: f32, w: f32, h: f32, reference: &Rect) -> Rect {
+        Rect {
+            x: x / reference.w,
+            y: y / reference.h,
+            w: w / reference.w,
+            h: h / reference.h,
+        }
+    }
+
+    /// Create a new rect from `i32` coordinates.
+    pub const fn new_i32(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x: x as f32,
+            y: y as f32,
+            w: w as f32,
+            h: h as f32,
+        }
+    }
+
+    /// Create a new `Rect` with all values zero.
+    pub const fn zero() -> Self {
+        Self::new(0.0, 0.0, 0.0, 0.0)
+    }
+
+    /// Creates a new `Rect` at `0,0` with width and height 1.0
+    pub const fn one() -> Self {
+        Self::new(0.0, 0.0, 1.0, 1.0)
+    }
+
+    /// Gets the `Rect`'s x and y coordinates as a `Point2`.
+    pub const fn point(&self) -> cgmath::Point2<f32> {
+        cgmath::Point2 {
+            x: self.x,
+            y: self.y,
+        }
+    }
+
+    /// Returns the left edge of the `Rect`
+    pub const fn left(&self) -> f32 {
+        self.x
+    }
+
+    /// Returns the right edge of the `Rect`
+    pub fn right(&self) -> f32 {
+        self.x + self.w
+    }
+
+    /// Returns the top edge of the `Rect`
+    pub const fn top(&self) -> f32 {
+        self.y
+    }
+
+    /// Returns the bottom edge of the `Rect`
+    pub fn bottom(&self) -> f32 {
+        self.y + self.h
+    }
+
+    /// Checks whether the `Rect` contains a `Point`
+    pub fn contains(&self, point: Vector2<f32>) -> bool {
+        point.x >= self.left()
+            && point.x <= self.right()
+            && point.y <= self.bottom()
+            && point.y >= self.top()
+    }
+
+    /// Checks whether the `Rect` contains a `Point`
+    pub fn contains_within(&self, point: Vector2<f32>, tolerance: f32) -> bool {
+        point.x >= self.left() - tolerance
+            && point.x <= self.right() + tolerance
+            && point.y <= self.bottom() + tolerance
+            && point.y >= self.top() - tolerance
+    }
+
+    /// Checks whether the `Rect` overlaps another `Rect`
+    pub fn overlaps(&self, other: &Rect) -> bool {
+        self.left() <= other.right()
+            && self.right() >= other.left()
+            && self.top() <= other.bottom()
+            && self.bottom() >= other.top()
+    }
+
+    /// Translates the `Rect` by an offset of (x, y)
+    pub fn translate(&mut self, offset: Vector2<f32>) {
+        self.x += offset.x;
+        self.y += offset.y;
+    }
+
+    /// Moves the `Rect`'s origin to (x, y)
+    pub fn move_to(&mut self, destination: Vector2<f32>) {
+        self.x = destination.x;
+        self.y = destination.y;
+    }
+
+    /// Scales the `Rect` by a factor of (sx, sy),
+    /// growing towards the bottom-left
+    pub fn scale(&mut self, sx: f32, sy: f32) {
+        self.w *= sx;
+        self.h *= sy;
+    }
+    /// Returns a new `Rect` that includes all points of these two `Rect`s.
+    pub fn combine_with(self, other: Rect) -> Rect {
+        let x = f32::min(self.x, other.x);
+        let y = f32::min(self.y, other.y);
+        let w = f32::max(self.right(), other.right()) - x;
+        let h = f32::max(self.bottom(), other.bottom()) - y;
+        Rect { x, y, w, h }
+    }
+}
