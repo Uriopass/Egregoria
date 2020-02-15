7722364e4c3171c078ffcbd9178907f6f89d4c8e
Paris DOUADY <paris.douady@hotmail.fr>
2020-02-15 16:47:31 +0100

    Fix car color langage

--- scale/src/cars/data.rs
+++ scale/src/cars/data.rs
@@ -198,15 +198,15 @@ impl CarComponent {
 
 pub fn get_random_car_color() -> Color {
     let car_colors: [(Color, f32); 9] = [
-        (Color::from_hex(0x22_22_22), 0.22),  // "Noir"
-        (Color::from_hex(0xff_ff_ff), 0.19),  // Blanc
-        (Color::from_hex(0x66_66_66), 0.17),  // Gris plus clair
-        (Color::from_hex(0xb8_b8_b8), 0.14),  // Argenté
-        (Color::from_hex(0x1a_3c_70), 0.1),   // Bleu
-        (Color::from_hex(0xd8_22_00), 0.1),   // Rouge
-        (Color::from_hex(0x7_c4_b24), 0.02),  // Marron
-        (Color::from_hex(0xd4_c6_78), 0.015), // Or
-        (Color::from_hex(0x72_cb_19), 0.015), // Vert
+        (Color::from_hex(0x22_22_22), 0.22),  // Black
+        (Color::from_hex(0xff_ff_ff), 0.19),  // White
+        (Color::from_hex(0x66_66_66), 0.17),  // Gray
+        (Color::from_hex(0xb8_b8_b8), 0.14),  // Silver
+        (Color::from_hex(0x1a_3c_70), 0.1),   // Blue
+        (Color::from_hex(0xd8_22_00), 0.1),   // Red
+        (Color::from_hex(0x7c_4b_24), 0.02),  // Brown
+        (Color::from_hex(0xd4_c6_78), 0.015), // Gold
+        (Color::from_hex(0x72_cb_19), 0.015), // Green
     ];
 
     let total: f32 = car_colors.iter().map(|x| x.1).sum();
