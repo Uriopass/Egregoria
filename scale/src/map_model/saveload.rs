a7d1771358dceeb94d23c07fe3116e8a842e77ac
Paris DOUADY <paris.douady@hotmail.fr>
2020-02-29 17:39:14 +0100

    Fix clippy warnings

--- scale/src/map_model/saveload.rs
+++ scale/src/map_model/saveload.rs
@@ -147,7 +147,7 @@ pub fn add_doublecircle(pos: Vector2<f32>, m: &mut Map) {
 pub fn add_grid(pos: Vector2<f32>, m: &mut Map) {
     let mut grid: [[Option<IntersectionID>; 10]; 10] = [[None; 10]; 10];
     for (y, l) in grid.iter_mut().enumerate() {
-        for (x, v) in l.into_iter().enumerate() {
+        for (x, v) in l.iter_mut().enumerate() {
             *v = Some(m.add_intersection(pos + Vector2::new(x as f32 * 70.0, y as f32 * 70.0)));
         }
     }
