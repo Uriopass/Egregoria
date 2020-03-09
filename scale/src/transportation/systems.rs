f6ee12257f0a05af01183bb96dff2910c1b7115a
Douady Pâris <paris.douady@hotmail.fr>
2020-03-09 10:14:20 +0100

    Rename cars to transportation

--- scale/src/transportation/systems.rs
+++ scale/src/transportation/systems.rs
@@ -1,14 +1,14 @@
-use crate::cars::data::CarComponent;
 use crate::engine_interaction::TimeInfo;
 use crate::map_model::Map;
 use crate::physics::PhysicsWorld;
 use crate::physics::{Kinematics, Transform};
+use crate::transportation::data::TransportComponent;
 use cgmath::{vec2, Angle, InnerSpace, Vector2};
 use specs::prelude::*;
 use specs::shred::PanicHandler;
 
 #[derive(Default)]
-pub struct CarDecision;
+pub struct TransportDecision;
 
 pub const CAR_ACCELERATION: f32 = 3.0;
 pub const CAR_DECELERATION: f32 = 9.0;
@@ -17,39 +17,43 @@ pub const OBJECTIVE_OK_DIST: f32 = 4.0;
 pub const ANG_ACC: f32 = 1.0;
 
 #[derive(SystemData)]
-pub struct CarDecisionSystemData<'a> {
+pub struct TransportDecisionSystemData<'a> {
     map: Read<'a, Map, PanicHandler>,
     time: Read<'a, TimeInfo>,
     coworld: Read<'a, PhysicsWorld, PanicHandler>,
     transforms: WriteStorage<'a, Transform>,
     kinematics: WriteStorage<'a, Kinematics>,
-    cars: WriteStorage<'a, CarComponent>,
+    transports: WriteStorage<'a, TransportComponent>,
 }
 
-impl<'a> System<'a> for CarDecision {
-    type SystemData = CarDecisionSystemData<'a>;
+impl<'a> System<'a> for TransportDecision {
+    type SystemData = TransportDecisionSystemData<'a>;
 
     fn run(&mut self, mut data: Self::SystemData) {
         let cow = data.coworld;
         let map = &*data.map;
         let time = data.time;
 
-        (&mut data.transforms, &mut data.kinematics, &mut data.cars)
+        (
+            &mut data.transforms,
+            &mut data.kinematics,
+            &mut data.transports,
+        )
             .join()
-            .for_each(|(trans, kin, car)| {
-                car.objective_update(&time, trans, &map);
-                car_physics(&cow, &map, &time, trans, kin, car);
+            .for_each(|(trans, kin, transport)| {
+                transport.objective_update(&time, trans, &map);
+                transport_physics(&cow, &map, &time, trans, kin, transport);
             });
     }
 }
 
-fn car_physics(
+fn transport_physics(
     coworld: &PhysicsWorld,
     map: &Map,
     time: &TimeInfo,
     trans: &mut Transform,
     kin: &mut Kinematics,
-    car: &mut CarComponent,
+    transport: &mut TransportComponent,
 ) {
     let direction = trans.direction();
     let speed: f32 = kin.velocity.magnitude() * kin.velocity.dot(direction).signum();
@@ -69,28 +73,28 @@ fn car_physics(
 
     let objs = neighbors.map(|obj| (obj.pos, coworld.get_obj(obj.id)));
 
-    car.calc_decision(map, speed, time, trans, objs);
+    transport.calc_decision(map, speed, time, trans, objs);
 
     let speed = speed
-        + ((car.desired_speed - speed)
+        + ((transport.desired_speed - speed)
             .min(time.delta * CAR_ACCELERATION)
             .max(-time.delta * CAR_DECELERATION));
 
     let max_ang_vel = (speed.abs() / MIN_TURNING_RADIUS).min(2.0);
 
-    let delta_ang = direction.angle(car.desired_dir);
+    let delta_ang = direction.angle(transport.desired_dir);
     let mut ang = Vector2::unit_x().angle(direction);
 
-    car.ang_velocity += time.delta * ANG_ACC;
-    car.ang_velocity = car
+    transport.ang_velocity += time.delta * ANG_ACC;
+    transport.ang_velocity = transport
         .ang_velocity
         .min(max_ang_vel)
         .min(3.0 * delta_ang.0.abs());
 
     ang.0 += delta_ang
         .0
-        .min(car.ang_velocity * time.delta)
-        .max(-car.ang_velocity * time.delta);
+        .min(transport.ang_velocity * time.delta)
+        .max(-transport.ang_velocity * time.delta);
     let direction = vec2(ang.cos(), ang.sin());
     trans.set_direction(direction);
     kin.velocity = direction * speed;
