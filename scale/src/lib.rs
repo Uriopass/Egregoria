d8cbed8015ff0b3b5397ecc13f6c9ec9b916e5e0
Paris DOUADY <paris.douady@hotmail.fr>
2020-03-15 02:36:55 +0100

    Add world ui test

--- scale/src/lib.rs
+++ scale/src/lib.rs
@@ -1,19 +1,22 @@
 #![windows_subsystem = "windows"]
 
-use crate::engine_interaction::{KeyboardInfo, MeshRenderEventReader, RenderStats, TimeInfo};
+use crate::engine_interaction::{
+    KeyboardInfo, MeshRenderEventReader, MouseButton, MouseInfo, RenderStats, TimeInfo,
+};
 use crate::geometry::gridstore::GridStore;
 use crate::gui::Gui;
 use crate::humans::HumanUpdate;
 use crate::interaction::{
-    FollowEntity, MovableSystem, SelectableAuraSystem, SelectableSystem, SelectedEntity,
+    FollowEntity, MovableSystem, MovedEvent, SelectableAuraSystem, SelectableSystem, SelectedEntity,
 };
-use crate::map_model::RoadGraphSynchronize;
+use crate::map_model::{LogicComponent, RoadGraphSynchronize, RoadGraphSynchronizeState};
 use crate::physics::systems::KinematicsApply;
-use crate::physics::Collider;
 use crate::physics::CollisionWorld;
+use crate::physics::{Collider, Transform};
 use crate::rendering::meshrender_component::MeshRender;
 use crate::transportation::systems::TransportDecision;
-use specs::{Dispatcher, DispatcherBuilder, World, WorldExt};
+use cgmath::InnerSpace;
+use specs::{Dispatcher, DispatcherBuilder, Join, World, WorldExt};
 
 #[macro_use]
 pub mod gui;
@@ -28,6 +31,7 @@ pub mod physics;
 pub mod rendering;
 pub mod transportation;
 pub use specs;
+use specs::shrev::EventChannel;
 
 pub fn dispatcher<'a>() -> Dispatcher<'a, 'a> {
     DispatcherBuilder::new()
@@ -49,9 +53,25 @@ pub fn dispatcher<'a>() -> Dispatcher<'a, 'a> {
         .build()
 }
 
+pub fn ad_hoc_systems(world: &mut World) {
+    let logic = world.read_component::<LogicComponent>();
+    let trans = world.read_component::<Transform>();
+    let mouse: &MouseInfo = &world.read_resource::<MouseInfo>();
+
+    if mouse.just_pressed.contains(&MouseButton::Left) {
+        for (t, l) in (&trans, &logic).join() {
+            if (mouse.unprojected - t.position()).magnitude() < l.radius {
+                let v = &l.on_click;
+                v(world);
+            }
+        }
+    }
+}
+
 pub fn setup(world: &mut World, dispatcher: &mut Dispatcher) {
     let collision_world: CollisionWorld = GridStore::new(50);
 
+    // Resources init
     world.insert(TimeInfo::default());
     world.insert(collision_world);
     world.insert(KeyboardInfo::default());
@@ -62,9 +82,17 @@ pub fn setup(world: &mut World, dispatcher: &mut Dispatcher) {
 
     world.register::<Collider>();
     world.register::<MeshRender>();
+    world.register::<LogicComponent>();
 
+    // Event channels init
     let reader = MeshRenderEventReader(world.write_storage::<MeshRender>().register_reader());
     world.insert(reader);
+
+    world.insert(EventChannel::<MovedEvent>::new());
+
+    // Systems state init
+    let s = RoadGraphSynchronizeState::new(world);
+    world.insert(s);
 
     dispatcher.setup(world);
     map_model::setup(world);
