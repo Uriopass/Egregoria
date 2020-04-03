0433a085fd138863e5cbd761e1add2c7609cf225
Paris DOUADY <paris.douady@hotmail.fr>
2020-04-03 17:30:19 +0200

    Add local imgui inspect

---
+++ imgui-inspect/src/default/default_bool.rs
@@ -0,0 +1,69 @@
+use super::*;
+
+impl InspectRenderDefault<bool> for bool {
+    fn render(
+        data: &[&bool],
+        label: &'static str,
+        _: &mut World,
+        ui: &imgui::Ui,
+        _args: &InspectArgsDefault,
+    ) {
+        if data.len() == 0 {
+            // Values are inconsistent
+            let style_token = ui.push_style_color(imgui::StyleColor::Text, [1.0, 0.0, 0.0, 1.0]);
+            ui.text(&imgui::im_str!("{}: ", label));
+            style_token.pop(ui);
+            return;
+        }
+
+        match get_same_or_none(data) {
+            Some(_v) => {
+                // Values are consistent
+                ui.text(&imgui::im_str!("{}: {}", label, data[0]))
+            }
+            None => {
+                // Values are inconsistent
+                let style_token =
+                    ui.push_style_color(imgui::StyleColor::Text, [1.0, 1.0, 0.0, 1.0]);
+                ui.text(&imgui::im_str!("{}: ", label));
+                style_token.pop(ui);
+            }
+        }
+    }
+
+    fn render_mut(
+        data: &mut [&mut bool],
+        label: &'static str,
+        _: &mut World,
+        ui: &imgui::Ui,
+        _args: &InspectArgsDefault,
+    ) -> bool {
+        let same_or_none_value = get_same_or_none_mut(data);
+
+        let mut value = match same_or_none_value {
+            Some(v) => v,
+            None => false, // Some reasonable default
+        };
+
+        let style_token = if same_or_none_value.is_none() {
+            // If values are inconsistent, push a style
+            Some(ui.push_style_color(imgui::StyleColor::Text, [1.0, 1.0, 0.0, 1.0]))
+        } else {
+            None
+        };
+
+        let mut changed = false;
+        if ui.checkbox(&imgui::im_str!("{}", label), &mut value) {
+            for d in data {
+                **d = value;
+                changed = true;
+            }
+        }
+
+        if let Some(style_token) = style_token {
+            style_token.pop(ui);
+        }
+
+        changed
+    }
+}
