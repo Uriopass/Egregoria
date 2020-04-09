8a1538a44c13e35f8bd86cf6051506979df29262
Paris DOUADY <paris.douady@hotmail.fr>
2020-04-09 23:42:16 +0200

    Add basic pedestrian movements and SFM

--- scale/src/pedestrians/data.rs
+++ scale/src/pedestrians/data.rs
@@ -1,21 +1,25 @@
 use crate::gui::InspectVec2;
 use cgmath::{vec2, Vector2};
 use imgui_inspect_derive::*;
+use rand_distr::Distribution;
 use serde::{Deserialize, Serialize};
 use specs::{Component, DenseVecStorage};
-
-pub(crate) const WALKING_SPEED: f32 = 3.0;
 
 #[derive(Clone, Serialize, Deserialize, Component, Inspect)]
 pub struct PedestrianComponent {
     #[inspect(proxy_type = "InspectVec2")]
     pub objective: Vector2<f32>,
+    pub walking_speed: f32,
 }
 
 impl Default for PedestrianComponent {
     fn default() -> Self {
         Self {
             objective: vec2(0.0, 0.0),
+            walking_speed: rand_distr::Normal::new(1.34f32, 0.26) // https://arxiv.org/pdf/cond-mat/9805244.pdf
+                .unwrap()
+                .sample(&mut rand::thread_rng())
+                .max(0.5),
         }
     }
 }
