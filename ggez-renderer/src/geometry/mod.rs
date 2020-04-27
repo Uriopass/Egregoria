1e6dfa0236c1f22d6361ae85b49ce58417abdfd4
Paris DOUADY <paris.douady@hotmail.fr>
2020-04-27 17:30:58 +0200

    Remove ggez renderer :tada:

--- ggez-renderer/src/geometry/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod camera;
-pub mod rect;
-pub mod tesselator;
--- ggez-renderer/src/rendering/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod camera_handler;
-pub mod instanced_render;
-pub mod meshrenderable;
-pub mod render_context;
-pub mod road_rendering;
-
-pub mod sorted_mesh_renderer;
