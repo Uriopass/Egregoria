e5a2f81e3e600663ce5ad08a331f7729bc01436c
Paris DOUADY <paris.douady@hotmail.fr>
2020-05-09 21:23:46 +0200

    Add spirv caching

--- wgpu-renderer/src/engine/shader.rs
+++ wgpu-renderer/src/engine/shader.rs
@@ -1,11 +1,21 @@
 use std::fs::File;
 use std::io::Read;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 pub struct CompiledShader(pub Vec<u32>);
 
 pub fn compile_shader(p: impl AsRef<Path>) -> CompiledShader {
     let p = p.as_ref();
+
+    let mut extension = p.extension().unwrap().to_string_lossy().into_owned();
+    extension.push_str(".spirv");
+    let compiled_path = p.with_extension(extension);
+
+    if let Ok(x) = File::open(compiled_path.clone()) {
+        let data = wgpu::read_spirv(x).unwrap();
+        return CompiledShader(data);
+    }
+
     let mut file = File::open(p).unwrap_or_else(|_| panic!("Failed to open {:?} shader file", p));
     let mut src = String::new();
     file.read_to_string(&mut src)
@@ -24,7 +34,14 @@ pub fn compile_shader(p: impl AsRef<Path>) -> CompiledShader {
         );
     };
 
-    let spirv = glsl_to_spirv::compile(&src, shader_type).unwrap();
+    let mut spirv = glsl_to_spirv::compile(&src, shader_type).unwrap();
+
+    std::io::copy(
+        &mut spirv,
+        &mut File::create(compiled_path).expect("Error opening the cached spirv file"),
+    )
+    .expect("Error trying to cache spirv output");
+
     let data = wgpu::read_spirv(&spirv).unwrap();
     CompiledShader(data)
 }
