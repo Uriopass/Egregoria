fcf63fca2a9f0a066ab9d302d737436f496d38ac
Paris DOUADY <paris.douady@hotmail.fr>
2020-05-10 10:36:17 +0200

    Don't crash when failing to cache shaders output

--- wgpu-renderer/src/engine/shader.rs
+++ wgpu-renderer/src/engine/shader.rs
@@ -1,26 +1,37 @@
+use glsl_to_spirv::SpirvOutput;
 use std::fs::File;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 
 pub struct CompiledShader(pub Vec<u32>);
 
+fn cache_filename(p: &Path) -> Option<PathBuf> {
+    let mut name = p.file_name()?.to_string_lossy().into_owned();
+    name.push_str(".spirv");
+
+    Some(p.parent()?.parent()?.join("compiled_shaders").join(name))
+}
+
+fn find_in_cache(compiled_path: &PathBuf) -> Option<CompiledShader> {
+    let x = File::open(&compiled_path).ok()?;
+    let data = wgpu::read_spirv(x).ok()?;
+
+    Some(CompiledShader(data))
+}
+
+fn save_to_cache(compiled_path: &PathBuf, spirv: &mut SpirvOutput) -> Option<()> {
+    std::fs::create_dir_all(compiled_path.parent()?).ok()?;
+    std::io::copy(spirv, &mut File::create(compiled_path).ok()?).ok()?;
+    Some(())
+}
+
 pub fn compile_shader(p: impl AsRef<Path>) -> CompiledShader {
     let p = p.as_ref();
 
-    let mut name = p.file_name().unwrap().to_string_lossy().into_owned();
-    name.push_str(".spirv");
+    let compiled_name = cache_filename(p);
 
-    let compiled_path = p
-        .parent()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .join("compiled_shaders")
-        .join(name);
-
-    if let Ok(x) = File::open(compiled_path.clone()) {
-        let data = wgpu::read_spirv(x).unwrap();
-        return CompiledShader(data);
+    if let Some(x) = compiled_name.as_ref().and_then(|x| find_in_cache(&x)) {
+        return x;
     }
 
     let mut file = File::open(p).unwrap_or_else(|_| panic!("Failed to open {:?} shader file", p));
@@ -43,11 +54,7 @@ pub fn compile_shader(p: impl AsRef<Path>) -> CompiledShader {
 
     let mut spirv = glsl_to_spirv::compile(&src, shader_type).unwrap();
 
-    std::io::copy(
-        &mut spirv,
-        &mut File::create(compiled_path).expect("Error opening the cached spirv file"),
-    )
-    .expect("Error trying to cache spirv output");
+    compiled_name.and_then(|x| save_to_cache(&x, &mut spirv));
 
     let data = wgpu::read_spirv(&spirv).unwrap();
     CompiledShader(data)
