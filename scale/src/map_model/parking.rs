b71519ac4c9400371cb669e6527258e73ada7c94
Paris DOUADY <paris.douady@hotmail.fr>
2020-06-17 20:11:42 +0200

    Add parking query near

--- scale/src/map_model/parking.rs
+++ scale/src/map_model/parking.rs
@@ -1,11 +1,18 @@
 use crate::geometry::Vec2;
-use crate::map_model::{Lane, LaneID, LaneKind};
+use crate::map_model::{Lane, LaneID, LaneKind, Map};
+use ordered_float::OrderedFloat;
 use serde::{Deserialize, Serialize};
-use slotmap::SecondaryMap;
+use slotmap::{new_key_type, SecondaryMap, SlotMap};
+use std::collections::HashSet;
+use std::sync::RwLock;
+
+new_key_type! {
+    pub struct ParkingSpotID;
+}
 
 pub const PARKING_SPOT_LENGTH: f32 = 6.0;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Copy, Clone, Serialize, Deserialize)]
 pub struct ParkingSpot {
     pub pos: Vec2,
     pub orientation: Vec2,
@@ -13,32 +20,89 @@ pub struct ParkingSpot {
 
 #[derive(Serialize, Deserialize, Default)]
 pub struct ParkingSpots {
-    spots: SecondaryMap<LaneID, Vec<ParkingSpot>>,
+    spots: SlotMap<ParkingSpotID, ParkingSpot>,
+    lane_spots: SecondaryMap<LaneID, Vec<ParkingSpotID>>,
+    reserved_spots: RwLock<HashSet<ParkingSpotID>>,
 }
 
 impl ParkingSpots {
     pub fn generate_spots(&mut self, lane: &Lane) {
         debug_assert!(matches!(lane.kind, LaneKind::Parking));
-        let n_spots = (lane.length / PARKING_SPOT_LENGTH) as i32;
-        let step = lane.length / n_spots as f32;
 
-        let spots = match self.spots.get_mut(lane.id) {
+        let lane_spots = match self.lane_spots.get_mut(lane.id) {
             Some(x) => x,
             None => {
-                self.spots.insert(lane.id, vec![]);
-                &mut self.spots[lane.id]
+                self.lane_spots.insert(lane.id, vec![]);
+                &mut self.lane_spots[lane.id]
             }
         };
 
-        spots.clear();
+        let n_spots = (lane.length / PARKING_SPOT_LENGTH) as i32;
+        let step = lane.length / n_spots as f32;
 
-        spots.extend(
+        for spot in lane_spots.drain(..) {
+            self.spots.remove(spot);
+        }
+
+        let spots = &mut self.spots;
+        lane_spots.extend(
             lane.points
                 .points_dirs_along((0..n_spots).map(|x| (x as f32 + 0.5) * step))
-                .map(|(pos, dir)| ParkingSpot {
-                    pos,
-                    orientation: dir,
+                .map(|(pos, dir)| {
+                    spots.insert(ParkingSpot {
+                        pos,
+                        orientation: dir,
+                    })
                 }),
         );
+    }
+
+    pub fn closest_available_spot(
+        &self,
+        lane: LaneID,
+        near: Vec2,
+        reserved_spots: &HashSet<ParkingSpotID>,
+    ) -> Option<ParkingSpotID> {
+        debug_assert!(self.lane_spots.contains_key(lane));
+        let spots = &self.spots;
+        self.lane_spots
+            .get(lane)?
+            .iter()
+            .filter(|&p| !reserved_spots.contains(p))
+            .min_by_key(|&&id| OrderedFloat(spots[id].pos.distance2(near)))
+            .copied()
+    }
+
+    pub fn query_near(&self, lane: LaneID, near: Vec2, map: &Map) -> Option<ParkingSpotID> {
+        let lane = map.lanes().get(lane)?;
+
+        let reserved_spots = self.reserved_spots.read().unwrap();
+        let depth = 2;
+
+        let mut potential = vec![lane];
+        let mut next = vec![];
+
+        for _ in 0..depth {
+            for lane in potential.drain(..) {
+                let parent = unwrap_or!(map.roads().get(lane.parent), continue);
+
+                let p = parent
+                    .parking_next_to(lane)
+                    .and_then(|x| self.closest_available_spot(x, near, &reserved_spots));
+                if let Some(spot) = p {
+                    drop(reserved_spots);
+                    self.reserved_spots.write().unwrap().insert(spot);
+                    return Some(spot);
+                }
+
+                next.extend(
+                    map.intersections()[lane.dst]
+                        .turns_from(lane.id)
+                        .map(|(turn, _)| &map.lanes()[turn.dst]),
+                )
+            }
+            std::mem::swap(&mut potential, &mut next);
+        }
+        None
     }
 }
