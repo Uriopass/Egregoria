b50fcf9b1d28ed173bff751265b167f0200c9d79
Paris DOUADY <paris.douady@hotmail.fr>
2020-06-24 00:30:23 +0200

    temporarily disable car loading

--- scale/src/vehicles/saveload.rs
+++ scale/src/vehicles/saveload.rs
@@ -1,6 +1,5 @@
 use crate::map_interaction::Itinerary;
 use crate::physics::Transform;
-use crate::vehicles::make_vehicle_entity;
 use crate::vehicles::VehicleComponent;
 use specs::{Join, World, WorldExt};
 use std::fs::File;
@@ -27,13 +26,14 @@ pub fn save(world: &mut World) {
     bincode::serialize_into(file, &comps).unwrap();
 }
 
-pub fn load(world: &mut World) {
+pub fn load(_world: &mut World) {
     let file = File::open(VEHICLE_FILENAME.to_string() + ".bc");
     if let Err(e) = file {
         println!("error while trying to load entities: {}", e);
         return;
     }
 
+    /*
     let des = bincode::deserialize_from(file.unwrap());
 
     let comps: Vec<(Transform, VehicleComponent, Itinerary)> = des.unwrap_or_default();
@@ -41,4 +41,5 @@ pub fn load(world: &mut World) {
     for (trans, car, it) in comps {
         make_vehicle_entity(world, trans, car, it);
     }
+     */
 }
