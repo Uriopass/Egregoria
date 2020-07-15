adb8e864af60764228dcb1f82a152a54121b4e20
Paris DOUADY <paris.douady@hotmail.fr>
2020-07-16 00:14:31 +0200

    gen houses from lua

--- map_model/src/housing.rs
+++ map_model/src/housing.rs
@@ -16,21 +16,9 @@ pub struct House {
     pub bcirc: Circle,
 }
 
-fn rand_in(min: f32, max: f32) -> f32 {
-    min + rand::random::<f32>() * (max - min)
-}
-
 impl House {
     pub fn try_make(map: &mut Map, at: Vec2, axis: Vec2) -> Option<HouseID> {
-        let mut exterior = if rand::random() {
-            Self::gen_exterior_sq()
-        } else {
-            Self::gen_exterior_corner()
-        };
-
-        if let Some(house) = mods::gen_house() {
-            exterior = house;
-        }
+        let mut exterior = mods::gen_house()?;
 
         exterior.rotate(axis);
         exterior.translate(at + axis * exterior.bcircle().radius);
@@ -51,32 +39,5 @@ impl House {
         } else {
             None
         }
-    }
-
-    fn gen_exterior_sq() -> Polygon {
-        let w = rand_in(12.0, 22.0);
-        let h = rand_in(10.0, 20.0);
-
-        let mut p = Polygon::rect(w, h);
-        p.translate(-p.barycenter());
-        p
-    }
-
-    fn gen_exterior_corner() -> Polygon {
-        let a = rand_in(10.0, 20.0);
-        let b = rand_in(10.0, 20.0);
-
-        let (w, h) = (a.max(b), a.min(b));
-
-        let mut p = Polygon::rect(w, h);
-
-        let corn_coeff = rand_in(0.5, 0.75);
-
-        let seg = rand_in(0.0, 3.99) as usize;
-        p.split_segment(seg, corn_coeff);
-        p.extrude(seg, rand_in(5.0, 10.0));
-
-        p.translate(-p.barycenter());
-        p
     }
 }
--- mods/src/stdlib.rs
+++ mods/src/stdlib.rs
@@ -1,5 +1,7 @@
 use geom::polygon::Polygon;
-use mlua::{Lua, UserData, UserDataMethods};
+use geom::Vec2;
+use mlua::prelude::LuaResult;
+use mlua::{Lua, MetaMethod, UserData, UserDataMethods};
 
 #[derive(Clone)]
 pub struct LuaPolygon(pub Polygon);
@@ -14,13 +16,66 @@ impl UserData for LuaPolygon {
             p.0.split_segment(seg as usize, coeff);
             Ok(())
         });
+        methods.add_method_mut("barycenter", |_, p, (): ()| Ok(LuaVec2(p.0.barycenter())));
+        methods.add_method_mut("translate", |_, p, vec: LuaVec2| {
+            p.0.translate(vec.0);
+            Ok(())
+        });
+        methods.add_method_mut("rotate", |_, p, cossin: LuaVec2| {
+            p.0.rotate(cossin.0);
+            Ok(())
+        });
     }
 }
 
-pub fn add_std(lua: &Lua) {
-    let poly_rect = lua
-        .create_function(|_, (w, h): (f32, f32)| Ok(LuaPolygon(Polygon::rect(w, h))))
-        .unwrap();
+#[derive(Clone, Copy)]
+pub struct LuaVec2(pub Vec2);
 
-    lua.globals().set("poly_rect", poly_rect).unwrap();
+impl UserData for LuaVec2 {
+    fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
+        methods.add_method("magnitude", |_, vec, ()| Ok(vec.0.magnitude()));
+
+        methods.add_meta_function(MetaMethod::Unm, |_, vec1: LuaVec2| Ok(LuaVec2(-vec1.0)));
+
+        methods.add_meta_function(MetaMethod::Add, |_, (vec1, vec2): (LuaVec2, LuaVec2)| {
+            Ok(LuaVec2(vec1.0 + vec2.0))
+        });
+
+        methods.add_meta_function(MetaMethod::Sub, |_, (vec1, vec2): (LuaVec2, LuaVec2)| {
+            Ok(LuaVec2(vec1.0 - vec2.0))
+        });
+
+        methods.add_meta_function(MetaMethod::Mul, |_, (vec1, scalar): (LuaVec2, f32)| {
+            Ok(LuaVec2(vec1.0 * scalar))
+        });
+
+        methods.add_meta_function(MetaMethod::Mul, |_, (scalar, vec1): (f32, LuaVec2)| {
+            Ok(LuaVec2(scalar * vec1.0))
+        });
+
+        methods.add_meta_function(MetaMethod::Mul, |_, (vec1, vec2): (LuaVec2, LuaVec2)| {
+            Ok(LuaVec2(vec1.0 * vec2.0))
+        });
+    }
+}
+
+macro_rules! std_add_fn {
+    ($l: expr, $e: expr) => {
+        $l.globals()
+            .set(stringify!($e), $l.create_function($e).unwrap())
+            .unwrap();
+    };
+}
+
+fn poly_rect(_: &Lua, (w, h): (f32, f32)) -> LuaResult<LuaPolygon> {
+    Ok(LuaPolygon(Polygon::rect(w, h)))
+}
+
+fn rand_in(_: &Lua, (min, max): (f32, f32)) -> LuaResult<f32> {
+    Ok(min + rand::random::<f32>() * (max - min))
+}
+
+pub fn add_std(lua: &Lua) {
+    std_add_fn!(lua, poly_rect);
+    std_add_fn!(lua, rand_in);
 }
