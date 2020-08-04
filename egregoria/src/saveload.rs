a6e82fdc58a9a6423fef8cdbcf3688608b512a5b
Paris DOUADY <paris.douady@hotmail.fr>
2020-08-04 23:09:40 +0200

    fix serialization

--- egregoria/src/saveload.rs
+++ egregoria/src/saveload.rs
@@ -6,23 +6,22 @@ fn filename(name: &'static str) -> String {
     format!("world/{}.bc", name)
 }
 
+fn create_file(path: String) -> Option<File> {
+    File::create(path).map_err(|e| println!("{}", e)).ok()
+}
+
 fn open_file(path: String) -> Option<File> {
-    match File::create(path) {
-        Ok(f) => Some(f),
-        Err(e) => {
-            println!("{}", e);
-            None
-        }
-    }
+    File::open(path).map_err(|e| println!("{}", e)).ok()
 }
 
 pub fn save<T: Serialize>(x: &T, name: &'static str) -> Option<()> {
     let _ = std::fs::create_dir("world");
 
-    let file = open_file(filename(name))?;
+    let file = create_file(filename(name))?;
 
     let _ = bincode::serialize_into(file, x)
         .map_err(|err| println!("Error while serializing {}: {}", name, err));
+    println!("Successfully saved {}", name);
     Some(())
 }
 
