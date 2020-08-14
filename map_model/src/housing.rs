134dea786f26f5f93d35fde3d2d1d96b68974a64
Paris Douady <paris.douady@hotmail.fr>
2020-08-14 16:15:32 +0200

    use lot instead of road to build house

--- map_model/src/housing.rs
+++ map_model/src/housing.rs
@@ -1,4 +1,4 @@
-use crate::{Map, ProjectKind};
+use crate::{Lot, LotID, Map, ProjectKind};
 use geom::polygon::Polygon;
 use geom::Vec2;
 use serde::{Deserialize, Serialize};
@@ -15,35 +15,18 @@ pub struct House {
 }
 
 impl House {
-    pub fn try_make(map: &mut Map, at: Vec2, axis: Vec2) -> Option<HouseID> {
+    pub fn make(map: &mut Map, id: LotID) -> HouseID {
+        let lot = &map.lots[id];
+        let at = lot.shape.center();
+        let axis = lot.road_edge.vec().perpendicular();
+
         let mut exterior = mods::gen_house()?;
 
         exterior.rotate(axis);
         exterior.translate(at + axis * exterior.bcircle().radius);
 
-        let bcirc = exterior.bcircle();
-
-        for obj in map.spatial_map.query_rect(exterior.bbox()) {
-            match obj {
-                ProjectKind::Road(r) => {
-                    let r = &map.roads[r];
-                    if r.project(bcirc.center).distance(bcirc.center) - r.width * 0.5 < bcirc.radius
-                    {
-                        return None;
-                    }
-                }
-                ProjectKind::House(h) => {
-                    let h = &map.houses[h];
-                    if h.exterior.intersects(&exterior) {
-                        return None;
-                    }
-                }
-                _ => {}
-            }
-        }
-
         let id = map.houses.insert_with_key(move |id| Self { id, exterior });
         map.spatial_map.insert_house(&map.houses[id]);
-        Some(id)
+        id
     }
 }
