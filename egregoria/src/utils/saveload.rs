ab06d8151a3ae75529d017d389df2d83e0609e83
Paris DOUADY <paris.douady@hotmail.fr>
2020-09-11 09:30:35 +0200

    fix bincode deserializer using wrong options

--- egregoria/src/utils/saveload.rs
+++ egregoria/src/utils/saveload.rs
@@ -1,17 +1,11 @@
-use bincode::DefaultOptions;
+use bincode::{DefaultOptions, Options};
 use serde::de::{DeserializeOwned, DeserializeSeed};
 use serde::Serialize;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 
-static USE_JSON: bool = true;
-
 fn filename(name: &'static str) -> String {
-    if USE_JSON {
-        format!("world/{}.json", name)
-    } else {
-        format!("world/{}.bc", name)
-    }
+    format!("world/{}.bc", name)
 }
 
 fn create_file(path: &str) -> Option<File> {
@@ -29,13 +23,8 @@ pub fn save<T: Serialize>(x: &T, name: &'static str) -> Option<()> {
 
     let w = BufWriter::new(file);
 
-    if USE_JSON {
-        let _ = serde_json::to_writer(w, x)
-            .map_err(|err| error!("failed serializing {}: {}", name, err));
-    } else {
-        let _ = bincode::serialize_into(w, x)
-            .map_err(|err| error!("failed serializing {}: {}", name, err));
-    }
+    let _ =
+        bincode::serialize_into(w, x).map_err(|err| error!("failed serializing {}: {}", name, err));
     info!("successfully saved {}", name);
     Some(())
 }
@@ -45,41 +34,28 @@ pub fn load_or_default<T: DeserializeOwned + Default>(name: &'static str) -> T {
 }
 
 pub fn load<T: DeserializeOwned>(name: &'static str) -> Option<T> {
-    let des = if USE_JSON {
-        serde_json::from_reader(load_reader(name)?)
-            .map_err(|err| error!("failed deserializing {}: {}", name, err))
-    } else {
-        bincode::deserialize_from(load_reader(name)?)
-            .map_err(|err| error!("failed deserializing {}: {}", name, err))
-    };
+    bincode::deserialize_from(load_reader(name)?)
+        .map_err(|err| error!("failed deserializing {}: {}", name, err))
+        .map(|x| {
+            info!("successfully loaded {}", name);
+            x
+        })
+        .ok()
+}
 
-    des.map(|x| {
+pub fn load_seed<S: DeserializeSeed<'static>>(name: &'static str, seed: S) -> Option<S::Value> {
+    seed.deserialize(&mut bincode::Deserializer::with_reader(
+        load_reader(name)?,
+        DefaultOptions::new()
+            .allow_trailing_bytes()
+            .with_fixint_encoding(),
+    ))
+    .map_err(|err| error!("failed deserializing {}: {}", name, err))
+    .map(|x| {
         info!("successfully loaded {}", name);
         x
     })
     .ok()
-}
-
-pub fn load_seed<S: DeserializeSeed<'static>>(name: &'static str, seed: S) -> Option<S::Value> {
-    let r = load_reader(name)?;
-
-    let des = if USE_JSON {
-        seed.deserialize(&mut serde_json::Deserializer::from_reader(r))
-            .map_err(|err| error!("failed deserializing {}: {}", name, err))
-            .ok()
-    } else {
-        seed.deserialize(&mut bincode::Deserializer::with_reader(
-            r,
-            DefaultOptions::new(),
-        ))
-        .map_err(|err| error!("failed deserializing {}: {}", name, err))
-        .ok()
-    };
-
-    des.map(|x| {
-        info!("successfully loaded {}", name);
-        x
-    })
 }
 
 pub fn load_reader(name: &'static str) -> Option<BufReader<File>> {
