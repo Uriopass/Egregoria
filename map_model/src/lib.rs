2f2643835d6d8f7e65b9bd0f830655bd11383a53
Paris DOUADY <paris.douady@hotmail.fr>
2020-10-07 18:36:20 +0200

    refactor map_model objects into module

--- map_model/src/lib.rs
+++ map_model/src/lib.rs
@@ -12,40 +12,52 @@ macro_rules! unwrap_or {
 #[macro_use]
 extern crate log;
 
-mod building;
-pub mod heightmap;
-mod intersection;
-mod lane;
+mod objects {
+    mod building;
+    mod intersection;
+    mod lane;
+    mod lot;
+    mod parking;
+    mod road;
+    mod turn;
+
+    pub use building::*;
+    pub use intersection::*;
+    pub use lane::*;
+    pub use lot::*;
+    pub use parking::*;
+    pub use road::*;
+    pub use turn::*;
+}
+
+pub use objects::*;
+
+mod procgen {
+    pub mod heightmap;
+    mod presets;
+
+    pub use presets::*;
+}
+
+pub use procgen::*;
+
 mod light_policy;
-mod lot;
 mod map;
-mod mapgen;
-mod parking;
 mod pathfinding;
-mod road;
 mod serializing;
 mod spatial_map;
 mod traffic_control;
 mod traversable;
-mod turn;
 mod turn_policy;
 
 // Use self or else it would be ambiguous with "pathfinding" crate
 pub use self::pathfinding::*;
-pub use building::*;
-pub use intersection::*;
-pub use lane::*;
 pub use light_policy::*;
-pub use lot::*;
 pub use map::*;
-pub use mapgen::*;
-pub use parking::*;
-pub use road::*;
 pub use serializing::*;
 pub use spatial_map::*;
 pub use traffic_control::*;
 pub use traversable::*;
-pub use turn::*;
 pub use turn_policy::*;
 
 pub const CROSSWALK_WIDTH: f32 = 4.0;
