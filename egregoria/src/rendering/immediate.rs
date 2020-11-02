05e4ae60ea124ba1850ba0a23a79d3c5d2b27f6f
Paris DOUADY <paris.douady@hotmail.fr>
2020-11-02 16:54:12 +0100

    immediate draw z support

--- egregoria/src/rendering/immediate.rs
+++ egregoria/src/rendering/immediate.rs
@@ -1,27 +1,38 @@
 use geom::{Color, Vec2};
 
 #[derive(Copy, Clone)]
-pub enum ImmediateOrder {
+pub enum OrderKind {
     Circle { pos: Vec2, size: f32 },
     Line { from: Vec2, to: Vec2 },
 }
 
+#[derive(Copy, Clone)]
+pub struct ImmediateOrder {
+    pub kind: OrderKind,
+    pub color: Color,
+    pub z: f32,
+}
+
 #[derive(Default)]
 pub struct ImmediateDraw {
-    pub orders: Vec<(ImmediateOrder, Color)>,
-    pub persistent_orders: Vec<(ImmediateOrder, Color)>,
+    pub orders: Vec<ImmediateOrder>,
+    pub persistent_orders: Vec<ImmediateOrder>,
 }
 
 pub struct ImmediateBuilder<'a> {
     draw: &'a mut ImmediateDraw,
     order: ImmediateOrder,
-    color: Color,
     persistent: bool,
 }
 
 impl<'a> ImmediateBuilder<'a> {
     pub fn color(&mut self, col: Color) -> &mut Self {
-        self.color = col;
+        self.order.color = col;
+        self
+    }
+
+    pub fn z(&mut self, z: f32) -> &mut Self {
+        self.order.z = z;
         self
     }
 
@@ -34,9 +45,9 @@ impl<'a> ImmediateBuilder<'a> {
 impl<'a> Drop for ImmediateBuilder<'a> {
     fn drop(&mut self) {
         if self.persistent {
-            self.draw.persistent_orders.push((self.order, self.color))
+            self.draw.persistent_orders.push(self.order)
         } else {
-            self.draw.orders.push((self.order, self.color))
+            self.draw.orders.push(self.order)
         }
     }
 }
@@ -45,8 +56,11 @@ impl ImmediateDraw {
     pub fn circle(&mut self, pos: Vec2, size: f32) -> ImmediateBuilder {
         ImmediateBuilder {
             draw: self,
-            order: ImmediateOrder::Circle { pos, size },
-            color: Color::WHITE,
+            order: ImmediateOrder {
+                kind: OrderKind::Circle { pos, size },
+                color: Color::WHITE,
+                z: 3.0,
+            },
             persistent: false,
         }
     }
@@ -54,8 +68,11 @@ impl ImmediateDraw {
     pub fn line(&mut self, from: Vec2, to: Vec2) -> ImmediateBuilder {
         ImmediateBuilder {
             draw: self,
-            order: ImmediateOrder::Line { from, to },
-            color: Color::WHITE,
+            order: ImmediateOrder {
+                kind: OrderKind::Line { from, to },
+                color: Color::WHITE,
+                z: 3.0,
+            },
             persistent: false,
         }
     }
