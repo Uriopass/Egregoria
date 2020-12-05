c584e75b27c31468e7d87d6468fd1ffb0b30ffba
Paris DOUADY <paris.douady@hotmail.fr>
2020-12-05 10:54:52 +0100

    add own logging facade

--- native_app/src/main.rs
+++ native_app/src/main.rs
@@ -1,56 +1,23 @@
 #![allow(clippy::too_many_arguments)]
 
 use crate::context::Context;
-use log::{Level, LevelFilter};
-use std::io::Write;
-use std::time::Instant;
+use crate::logger::MyLog;
+use log::LevelFilter;
 
 mod audio;
 mod context;
 mod game_loop;
 mod gui;
 mod input;
+mod logger;
 mod rendering;
 
 fn main() {
-    let start = Instant::now();
-    env_logger::builder()
-        .filter(None, LevelFilter::Info)
-        .filter(Some("wgpu_core"), LevelFilter::Warn)
-        .filter(Some("gfx_memory"), LevelFilter::Warn)
-        .filter(Some("gfx_backend_vulkan"), LevelFilter::Warn)
-        .filter(Some("gfx_descriptor"), LevelFilter::Warn)
-        .format(move |f, r| {
-            if std::thread::panicking() {
-                return Ok(());
-            }
-            let time = Instant::now().duration_since(start).as_micros();
-            if r.level() > Level::Warn {
-                let module_path = r
-                    .module_path_static()
-                    .and_then(|x| x.split(':').last())
-                    .unwrap_or_default();
-                writeln!(
-                    f,
-                    "[{:9} {:5} {:12}] {}",
-                    time,
-                    r.metadata().level().to_string(),
-                    module_path,
-                    r.args()
-                )
-            } else {
-                writeln!(
-                    f,
-                    "[{:9} {:5} {}:{}] {}",
-                    time,
-                    r.metadata().level().to_string(),
-                    r.file().unwrap_or_default(),
-                    r.line().unwrap_or_default(),
-                    r.args()
-                )
-            }
-        })
-        .init();
+    let leaked = Box::leak(Box::new(MyLog::new()));
+    log::set_logger(leaked).unwrap();
+    log::set_max_level(LevelFilter::Debug);
+    log_panics::init();
+
     let mut ctx = Context::new();
 
     let state = game_loop::State::new(&mut ctx);
