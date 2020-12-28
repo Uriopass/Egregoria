9857571ce43cd51dc5dd984f020e4ada78380427
Paris DOUADY <paris.douady@hotmail.fr>
2020-12-28 01:46:40 +0100

    start skeleton rewrite of economy

--- egregoria/src/economy/mod.rs
+++ egregoria/src/economy/mod.rs
@@ -1,38 +1,8 @@
-use crate::SoulID;
-use derive_more::{Add, AddAssign, Sub, SubAssign};
-
 mod market;
 
 pub use market::*;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Add, AddAssign, Sub, SubAssign)]
-pub struct Money(pub i32);
-
-#[derive(Copy, Clone, Default, Debug, Add, AddAssign, Sub, SubAssign)]
-pub struct Goods {
-    pub food: i32,
-}
-
-impl Goods {
-    pub fn is_smaller(&self, delta: &Goods) -> bool {
-        self.food <= delta.food
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
-pub struct Transaction {
-    pub cost: Money,
-    pub delta: Goods,
-}
-
-pub struct EconomicAgent {
-    pub id: SoulID,
-    pub money: Money,
-    pub goods: Goods,
-}
-
-impl EconomicAgent {
-    pub fn new(id: SoulID, money: Money, goods: Goods) -> Self {
-        EconomicAgent { id, money, goods }
-    }
-}
+pub struct Wheat;
+pub struct Flour;
+pub struct Bread;
+pub struct JobApplication;
