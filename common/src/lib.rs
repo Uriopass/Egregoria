2e2d28ba1c5c4065c774ef99c91806b1cf08fe8f
Paris DOUADY <paris.douady@hotmail.fr>
2021-02-26 18:09:08 +0100

    remove get mut pair

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 #[macro_export]
 macro_rules! unwrap_or {
     ($e: expr, $t: expr) => {
@@ -35,20 +33,4 @@ pub enum AudioKind {
     Music,
     Effect,
     Ui,
-}
-
-pub fn get_mut_pair<'a, K, V>(
-    conns: &'a mut HashMap<K, V>,
-    a: &K,
-    b: &K,
-) -> Option<(&'a mut V, &'a mut V)>
-where
-    K: std::fmt::Debug + Eq + std::hash::Hash,
-{
-    unsafe {
-        let a = conns.get_mut(a)? as *mut _;
-        let b = conns.get_mut(b)? as *mut _;
-        assert_ne!(a, b, "The two keys must not resolve to the same value");
-        Some((&mut *a, &mut *b))
-    }
 }
