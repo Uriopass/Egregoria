3254041f68f7c82cab8e55bd4e96a66e1053c287
Paris DOUADY <paris.douady@hotmail.fr>
2021-03-03 19:22:49 +0100

    bigger config

--- native_app/src/gui/windows/config.rs
+++ native_app/src/gui/windows/config.rs
@@ -1,19 +1,26 @@
 use common::Config;
 use egregoria::Egregoria;
-use imgui::Ui;
+use imgui::{Condition, Ui};
 use imgui_inspect::{InspectArgsDefault, InspectRenderDefault};
 
 pub fn config(window: imgui::Window, ui: &Ui, _goria: &mut Egregoria) {
-    window.build(&ui, || {
-        let mut config = (**common::config()).clone();
+    window
+        .size([600.0, 500.0], Condition::FirstUseEver)
+        .build(&ui, || {
+            let mut config = (**common::config()).clone();
 
-        let args = InspectArgsDefault {
-            header: Some(false),
-            indent_children: Some(false),
-            ..InspectArgsDefault::default()
-        };
-        if <Config as InspectRenderDefault<Config>>::render_mut(&mut [&mut config], "", ui, &args) {
-            common::update_config(config);
-        }
-    });
+            let args = InspectArgsDefault {
+                header: Some(false),
+                indent_children: Some(false),
+                ..InspectArgsDefault::default()
+            };
+            if <Config as InspectRenderDefault<Config>>::render_mut(
+                &mut [&mut config],
+                "",
+                ui,
+                &args,
+            ) {
+                common::update_config(config);
+            }
+        });
 }
