1d37a0d83995c99369e2f747b23b093386cae621
Paris DOUADY <paris.douady@hotmail.fr>
2021-04-03 10:51:52 +0200

    use pkg version from goria version

--- goria_version/build.rs
+++
@@ -1,9 +0,0 @@
-use vergen::Config;
-
-fn main() {
-    let mut c = Config::default();
-    *c.git_mut().commit_timestamp_mut() = false;
-    *c.git_mut().branch_mut() = false;
-    *c.git_mut().sha_kind_mut() = vergen::ShaKind::Short;
-    let _ = vergen::vergen(c);
-}
--- goria_version/src/lib.rs
+++ goria_version/src/lib.rs
@@ -1,7 +1,2 @@
 #[allow(clippy::manual_unwrap_or)]
-pub const VERSION: &str = {
-    match option_env!("VERGEN_GIT_SHA") {
-        Some(x) => x,
-        None => "undefined_version",
-    }
-};
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
