a1fedcd8669fb21f2fa82f74f11c719f659e9d2c
Paris DOUADY <paris.douady@hotmail.fr>
2021-04-11 18:01:26 +0200

    don't insert config at compile time, messes up compile time

--- common/build.rs
+++
@@ -1,8 +0,0 @@
-use std::env;
-
-fn main() {
-    if let Ok(x) = std::fs::read("../assets/config.json") {
-        let _ = std::fs::write(env::var("OUT_DIR").unwrap() + "/config.json", x);
-    }
-    println!("cargo:rerun-if-changed=../assets/config.json");
-}
