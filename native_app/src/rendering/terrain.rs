9e37ce21c376f2fd603833427727c85b7e36efa7
Paris DOUADY <paris.douady@hotmail.fr>
2021-04-18 23:37:49 +0200

    add terrain lod generation

--- native_app/src/rendering/terrain.rs
+++ native_app/src/rendering/terrain.rs
@@ -1,30 +1,31 @@
 use crate::uiworld::UiWorld;
 use common::{FastMap, Z_TERRAIN};
 use geom::{vec2, LinearColor};
+use std::mem::MaybeUninit;
 use wgpu_engine::pbuffer::PBuffer;
 use wgpu_engine::wgpu::BufferUsage;
 use wgpu_engine::IndexType;
 use wgpu_engine::{ColoredVertex, FrameContext, GfxContext, Mesh};
 
 const CHUNK_SIZE: f32 = 1000.0;
-const RESOLUTION: usize = 20;
+const RESOLUTION: usize = 40;
+const LOD: usize = 4;
+
 struct TerrainChunk {
-    mesh: Mesh,
+    lods: [Mesh; LOD],
 }
 
 pub struct TerrainRender {
     chunks: FastMap<(i32, i32), TerrainChunk>,
-    indices: PBuffer,
-    n_indices: u32,
+    indices: [(PBuffer, usize); LOD],
 }
 
 impl TerrainRender {
     pub fn new(gfx: &mut GfxContext) -> Self {
-        let (indices, n_indices) = Self::generate_indices(gfx);
+        let indices = Self::generate_indices(gfx);
         let mut me = TerrainRender {
             chunks: Default::default(),
             indices,
-            n_indices: n_indices as u32,
         };
 
         for y in -10..10 {
@@ -36,76 +37,111 @@ impl TerrainRender {
     }
 
     fn generate(&mut self, gfx: &mut GfxContext, x: i32, y: i32) {
-        let mut mesh = Vec::with_capacity((RESOLUTION + 1) * (RESOLUTION + 1));
+        let mut v = vec![];
+        for lod in 0..LOD {
+            let resolution = RESOLUTION / (1 << lod);
 
-        let offset = vec2(x as f32, y as f32) * CHUNK_SIZE;
+            let mut mesh = Vec::with_capacity((resolution + 1) * (resolution + 1));
 
-        for y in 0..=RESOLUTION {
-            let y = y as f32 / RESOLUTION as f32;
-            for x in 0..=RESOLUTION {
-                let x = x as f32 / RESOLUTION as f32;
-                let pos = vec2(x, y);
-                let pos = pos * CHUNK_SIZE + offset;
+            let offset = vec2(x as f32, y as f32) * CHUNK_SIZE;
 
-                let height = map_model::procgen::heightmap::height(pos);
+            for y in 0..=resolution {
+                let y = y as f32 / resolution as f32;
+                for x in 0..=resolution {
+                    let x = x as f32 / resolution as f32;
+                    let pos = vec2(x, y);
+                    let pos = pos * CHUNK_SIZE + offset;
 
-                let col = if height < 0.1 {
-                    common::config().sea_col
-                } else if height < 0.12 {
-                    common::config().sand_col
-                } else {
-                    common::config().grass_col
-                };
-                let col = LinearColor::from(col);
+                    let height = map_model::procgen::heightmap::height(pos);
 
-                mesh.push(ColoredVertex {
-                    position: [pos.x, pos.y, Z_TERRAIN],
-                    color: col.into(),
-                })
+                    let col: LinearColor = if height < 0.1 {
+                        common::config().sea_col.into()
+                    } else if height < 0.12 {
+                        common::config().sand_col.into()
+                    } else {
+                        0.37 * LinearColor::from(common::config().grass_col)
+                    };
+
+                    mesh.push(ColoredVertex {
+                        position: [pos.x, pos.y, Z_TERRAIN],
+                        color: col.into(),
+                    })
+                }
             }
+
+            let (ref indice, n_indices) = self.indices[lod];
+
+            let mut vbuf = PBuffer::new(BufferUsage::VERTEX);
+            vbuf.write(gfx, bytemuck::cast_slice(&mesh));
+            let m = Mesh {
+                vertex_buffer: vbuf.inner().expect("vertices were generated"),
+                index_buffer: indice.inner().expect("indices was generated"),
+                n_indices: n_indices as u32,
+            };
+            v.push(m);
         }
 
-        let mut vbuf = PBuffer::new(BufferUsage::VERTEX);
-        vbuf.write(gfx, bytemuck::cast_slice(&mesh));
-
         let chunk = TerrainChunk {
-            mesh: Mesh {
-                vertex_buffer: vbuf.inner().expect("vertices were generated"),
-                index_buffer: self.indices.inner().expect("indices was generated"),
-                n_indices: self.n_indices,
-            },
+            lods: collect_arr4(v),
         };
-
         self.chunks.insert((x, y), chunk);
     }
 
-    fn generate_indices(gfx: &GfxContext) -> (PBuffer, usize) {
-        let mut indices: Vec<IndexType> = Vec::with_capacity(6 * RESOLUTION * RESOLUTION);
+    fn generate_indices(gfx: &GfxContext) -> [(PBuffer, usize); 4] {
+        let mut v = vec![];
+        for lod in 0..LOD {
+            let resolution = RESOLUTION / (1 << lod);
+            let mut indices: Vec<IndexType> = Vec::with_capacity(6 * resolution * resolution);
 
-        let w = (RESOLUTION + 1) as IndexType;
-        for y in 0..RESOLUTION as IndexType {
-            for x in 0..RESOLUTION as IndexType {
-                let idx = y * w + x;
-                indices.push(idx);
-                indices.push(idx + 1);
-                indices.push(idx + w + 1);
+            let w = (resolution + 1) as IndexType;
+            for y in 0..resolution as IndexType {
+                for x in 0..resolution as IndexType {
+                    let idx = y * w + x;
+                    indices.push(idx);
+                    indices.push(idx + 1);
+                    indices.push(idx + w + 1);
 
-                indices.push(idx);
-                indices.push(idx + w + 1);
-                indices.push(idx + w);
+                    indices.push(idx);
+                    indices.push(idx + w + 1);
+                    indices.push(idx + w);
+                }
             }
+
+            let l = indices.len();
+
+            let mut buf = PBuffer::new(BufferUsage::INDEX);
+            buf.write(gfx, bytemuck::cast_slice(&indices));
+            v.push((buf, l));
         }
-
-        let l = indices.len();
-
-        let mut buf = PBuffer::new(BufferUsage::INDEX);
-        buf.write(gfx, bytemuck::cast_slice(&indices));
-        (buf, l)
+        collect_arr4(v)
     }
 
     pub fn render(&mut self, _uiw: &UiWorld, fctx: &mut FrameContext) {
         for chunk in self.chunks.values() {
-            fctx.objs.push(Box::new(chunk.mesh.clone()))
+            fctx.objs.push(Box::new(chunk.lods[0].clone()))
         }
     }
+}
+
+fn collect_arr4<T>(x: impl IntoIterator<Item = T>) -> [T; LOD] {
+    let mut arr = MaybeUninit::uninit();
+
+    let mut ptr = arr.as_mut_ptr() as *mut T;
+    let mut i = 0;
+    for v in x {
+        if i == LOD {
+            panic!("not 4")
+        }
+        unsafe {
+            ptr.write(v);
+            ptr = ptr.add(1);
+        }
+        i += 1;
+    }
+
+    if i < LOD {
+        panic!("not 4")
+    }
+
+    unsafe { arr.assume_init() }
 }
