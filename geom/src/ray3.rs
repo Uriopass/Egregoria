6f1831916a3d4221664d6f1ec3babba4e24c4845
Paris DOUADY <paris.douady@hotmail.fr>
2021-04-19 08:52:33 +0200

    add plane and ray3 and plane <-> ray3 inter

---
+++ geom/src/ray3.rs
@@ -0,0 +1,27 @@
+use super::Vec3;
+use crate::Plane;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Ray3 {
+    pub from: Vec3,
+    pub dir: Vec3,
+}
+
+impl Ray3 {
+    pub fn new(from: Vec3, dir: Vec3) -> Self {
+        Ray3 { from, dir }
+    }
+
+    pub fn intersection_plane(&self, p: &Plane) -> Option<Vec3> {
+        // assuming vectors are all normalized
+        let denom = p.n.dot(self.dir);
+        if denom > 1e-7 {
+            let diff = p.p - self.from;
+            let t = diff.dot(p.n) / denom;
+            if t >= 0.0 {
+                return Some(self.from + self.dir * t);
+            }
+        }
+        None
+    }
+}
