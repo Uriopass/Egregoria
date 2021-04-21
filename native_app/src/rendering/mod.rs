3096b3f9b34499161cb9bbe9c1c8e63f8a542121
Paris DOUADY <paris.douady@hotmail.fr>
2021-04-21 18:49:13 +0200

    remove background

--- native_app/src/rendering/mod.rs
+++ native_app/src/rendering/mod.rs
@@ -1,4 +1,3 @@
-pub use background::*;
 pub use camera_handler::*;
 pub use camera_handler_3d::*;
 pub use instanced_render::*;
@@ -6,7 +5,6 @@ pub use map_rendering::*;
 pub use mesh_renderer::*;
 pub use terrain::*;
 
-mod background;
 mod camera_handler;
 mod camera_handler_3d;
 pub mod imgui_wrapper;
