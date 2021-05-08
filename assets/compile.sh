edabfe7495c1f2e80ce57bae4019bf967ee46f22
Paris DOUADY <paris.douady@hotmail.fr>
2021-05-08 23:02:56 +0200

    sun's power is proportional to height

--- assets/compile.sh
+++ assets/compile.sh
@@ -2,9 +2,9 @@
 mkdir -p compiled_shaders
 cd shaders || exit
 for file in *.frag; do
-  glslc -O -o "../compiled_shaders/$file.spirv" "$file" &
+  glslc -g -O -o "../compiled_shaders/$file.spirv" "$file" &
 done
 for file in *.vert; do
-  glslc -O -o "../compiled_shaders/$file.spirv" "$file" &
+  glslc -g -O -o "../compiled_shaders/$file.spirv" "$file" &
 done
 wait
