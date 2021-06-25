7f35e26c9917c60840b19c2cfa7af1e1d6e1eff3
Paris DOUADY <paris.douady@hotmail.fr>
2021-06-25 06:46:33 +0200

    use spv not spirv extension

--- assets/compile.sh
+++ assets/compile.sh
@@ -2,9 +2,9 @@
 mkdir -p compiled_shaders
 cd shaders || exit
 for file in *.frag; do
-  glslc -g -O -o "../compiled_shaders/$file.spirv" "$file" &
+  glslc -g -O -o "../compiled_shaders/$file.spv" "$file" &
 done
 for file in *.vert; do
-  glslc -g -O -o "../compiled_shaders/$file.spirv" "$file" &
+  glslc -g -O -o "../compiled_shaders/$file.spv" "$file" &
 done
 wait
