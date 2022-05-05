90fc47edfd6d5aab3a6248d794a0f951fb67b535
Paris DOUADY <paris.douady@hotmail.fr>
2022-05-05 23:59:06 +0200

    fix headless

--- headless/src/main.rs
+++ headless/src/main.rs
@@ -1,9 +1,8 @@
 use common::logger::MyLog;
 use common::unwrap_or;
 use egregoria::engine_interaction::WorldCommands;
-use egregoria::{Egregoria, SerPreparedEgregoria};
+use egregoria::Egregoria;
 use networking::{Frame, Server, ServerConfiguration, ServerPollResult};
-use std::convert::TryFrom;
 use std::time::{Duration, Instant};
 use structopt::StructOpt;
 
@@ -41,35 +40,26 @@ fn main() {
 
     let mut sched = Egregoria::schedule();
 
-    let mut server: Server<SerPreparedEgregoria, WorldCommands> =
-        match Server::start(ServerConfiguration {
-            start_frame: Frame(w.get_tick()),
-            period: Duration::from_millis(opt.timestep),
-            port: opt.port,
-            virtual_client: None,
-            version: goria_version::VERSION.to_string(),
-            always_run: opt.always_run,
-        }) {
-            Ok(x) => x,
-            Err(e) => {
-                log::error!("could not start server: {}", e);
-                return;
-            }
-        };
+    let mut server: Server<Egregoria, WorldCommands> = match Server::start(ServerConfiguration {
+        start_frame: Frame(w.get_tick()),
+        period: Duration::from_millis(opt.timestep),
+        port: opt.port,
+        virtual_client: None,
+        version: goria_version::VERSION.to_string(),
+        always_run: opt.always_run,
+    }) {
+        Ok(x) => x,
+        Err(e) => {
+            log::error!("could not start server: {}", e);
+            return;
+        }
+    };
     log::info!("server started!");
 
     let mut last_saved = Instant::now();
 
     loop {
-        if let ServerPollResult::Input(inputs) = server.poll(
-            &|| {
-                (
-                    SerPreparedEgregoria::try_from(&w).expect("could not serialize server"),
-                    Frame(w.get_tick()),
-                )
-            },
-            None,
-        ) {
+        if let ServerPollResult::Input(inputs) = server.poll(&w, Frame(w.get_tick()), None) {
             for frame in inputs {
                 assert_eq!(frame.frame.0, w.get_tick() + 1);
                 let merged = frame.inputs.into_iter().map(|x| x.inp).collect();
