8dcd7bd48ed0e14098a45e667462cdfe23ed1ea1
Paris DOUADY <paris.douady@hotmail.fr>
2022-11-24 20:33:36 +0100

    config is scrollable

--- native_app/src/gui/windows/config.rs
+++ native_app/src/gui/windows/config.rs
@@ -4,16 +4,19 @@ use egregoria::Egregoria;
 use egui_inspect::{Inspect, InspectArgs};
 
 pub(crate) fn config(window: egui::Window<'_>, ui: &egui::Context, _: &mut UiWorld, _: &Egregoria) {
-    window.default_size([600.0, 500.0]).show(ui, |ui| {
-        let mut config = (**common::config()).clone();
+    window
+        .default_size([600.0, 500.0])
+        .vscroll(true)
+        .show(ui, |ui| {
+            let mut config = (**common::config()).clone();
 
-        let args = InspectArgs {
-            header: Some(false),
-            indent_children: Some(false),
-            ..InspectArgs::default()
-        };
-        if <Config as Inspect<Config>>::render_mut(&mut config, "", ui, &args) {
-            common::update_config(config);
-        }
-    });
+            let args = InspectArgs {
+                header: Some(false),
+                indent_children: Some(false),
+                ..InspectArgs::default()
+            };
+            if <Config as Inspect<Config>>::render_mut(&mut config, "", ui, &args) {
+                common::update_config(config);
+            }
+        });
 }
